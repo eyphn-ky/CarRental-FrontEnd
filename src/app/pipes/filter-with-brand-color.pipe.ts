f0cf15f1b4ed63cf84d5e3642d7b37023e82a7b0
eyphn-ky <eyuphan.kaya34@gmail.com>
2021-06-03 20:34:16 +0300

    Added Car Details

---
+++ src/app/pipes/filter-with-brand-color.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Car } from '../models/car';
+
+@Pipe({
+  name: 'filterWithBrandColor'
+})
+export class FilterWithBrandColorPipe implements PipeTransform {
+
+  transform(value:Car[] ,brandName:string,colorName:string){
+    return brandName?value.filter((p:Car)=>p.brandName.indexOf(brandName)!==-1):value;
+  }
+
+}
